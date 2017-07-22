1982aa8fab4c3d2cd075d2ee000e6b53f579273d
Tommy <tommy_tseng@ubiqconn.com>
2017-07-22 18:29:32 +0800

    implement echo reply

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let bodyParser = require('body-parser')
 let request = require('request')
 let app = express()
 
-const CHANNEL_ACCESS_TOKEN = 'Your_Channel_Access_Token'
+const CHANNEL_ACCESS_TOKEN = 'OvI4YotQg3BBfTDa9fz5I9vcM6VbbgxgPLN/MaqH0uY0L2o6fY9Z5ily+uxjAbBq09cHi9ttLWHkVJD01TcsnHyEfmKPbomITsK34XCDCziywLPhH+xtcBMa5vO0UDbT2clDbJM7Rx2tgDTV0tEFXwdB04t89/1O/w1cDnyilFU='
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -13,7 +13,14 @@ app.listen(PORT, function () {
 })
 
 // handler receiving messages
+// return should send response, if not ,line will not send because line think you not finish  
 app.post('/', function (req, res) {
+    let body  = req.body
+    let text  = body.events[0].message.text
+    let replyToken = body.events[0].replyToken
+    sendMessage(replyToken, text)
+    console.log(JSON.stringify(body,null,2))
+    res.send('')
 })
 
 // generic function sending messages
